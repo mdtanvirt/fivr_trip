24f5355978ba3376ad26dccad1cc82147470d79e
Tanvir Rahman <trahman@paradynamix.com>
2022-10-11 18:39:22 +0600

    Dash plotly initiationation

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, html, dcc
+from dash import Dash, dcc, html
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
+import json
 
 app = Dash(__name__)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+import json
+with open('trips.json', 'r') as f:
+    data = json.loads(f.read())
 
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+# Normalize data
+df = pd.json_normalize(data, record_path =['features'])
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+# Explore for Street names
+gcod_street_df = df.explode('properties.streetnames')
+# Remove column name 'geometry.coordinates'
+gcod_street_df = gcod_street_df.drop(['geometry.coordinates'], axis=1)
 
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
+street_name_list = df["properties.streetnames"].tolist()
 
+
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+
+app.layout = html.Div([
     dcc.Graph(
-        id='example-graph',
+        id='life-exp-vs-gdp',
         figure=fig
     )
 ])
