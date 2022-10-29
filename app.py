179770ae547f14222043593e6c3b57580c8b41ab
Tanvir Rahman <trahman@paradynamix.com>
2022-10-29 16:28:16 +0600

    Initiate streamlit app

--- app.py
+++ app.py
@@ -1,40 +1,28 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+import streamlit as st
+from streamlit_option_menu import option_menu
 
-from dash import Dash, dcc, html
-import plotly.express as px
-import plotly.graph_objects as go
-import pandas as pd
-import numpy as np
-import json
+st.set_page_config(layout="wide")
 
-app = Dash(__name__)
+# Hide streamlit default menu and footer from the template
+hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden}
+    footer {visibility: hidden}
+    header {visibility: hidden}
+    </style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
-import json
-with open('trips.json', 'r') as f:
-    data = json.loads(f.read())
+st.header("Trip analysis")
+with st.sidebar:
+    nav_menu = option_menu("Main Menu", ["Dashboard", "Map", 'Settings'], 
+        icons=['clipboard-data', 'map', 'gear'], menu_icon="cast", default_index=0)
 
-# Normalize data
-df = pd.json_normalize(data, record_path =['features'])
+if nav_menu == "Dashboard":
+    st.write("Analysis Dashboard")
 
-# Explore for Street names
-gcod_street_df = df.explode('properties.streetnames')
-# Remove column name 'geometry.coordinates'
-gcod_street_df = gcod_street_df.drop(['geometry.coordinates'], axis=1)
+elif nav_menu == "Map":
+    st.write("Data exploration with map")
 
-street_name_list = df["properties.streetnames"].tolist()
-
-
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
-
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
-    )
-])
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
+elif nav_menu == "Settings":
+    st.write("Raw data")
